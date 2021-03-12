bb03a6cc964d1dd478da9f71c481d29520b6ff50
heavymetalfan1123 <72998757+heavymetalfan1123@users.noreply.github.com>
2021-03-13 00:35:50 +0300

    a
    a

---
+++ index.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="initial-scale=1.0">
+  <title>index</title>
+  
+  
+</head>
+<body class="body page-index clearfix">
+  <div class="container container-1 clearfix">
+    <div class="element element-1"></div>
+    <?php
+	    echo "<p class='text'>здравствуйте это мой сайт порфолио</p>";
+    ?>
+  </div>
+  <div class="container container-2">
+    <div class="element element-2"></div>
+  </div>
+</body>
+</html>
+<style>
+body {
+  background-color: rgb(255, 255, 255);
+  font: normal 3.25em/1.68 'Showcard Gothic';
+  color: rgb(255, 255, 255);
+}
+
+.container {
+  float: left;
+}
+
+.element {
+  float: left;
+}
+
+.text {
+  position: relative;
+  float: left;
+  clear: both;
+  z-index: 1;
+  width: 74.3933054393%;
+  min-height: 85px;
+  margin: -115px 0 0 14.0585774058%;
+  background-color: rgba(255, 255, 255, 0);
+  font-size: 1em;
+  letter-spacing: 1px;
+  color: rgb(255, 255, 255);
+}
+
+.container-1 {
+  width: 99.5833333333%;
+  height: 172px;
+  margin: -20px 0 0 0.41667%;
+}
+
+.container-2 {
+  clear: both;
+  width: 80%;
+  height: 612px;
+  margin: 13px 0 0 10%;
+}
+
+.element-1 {
+  position: relative;
+  z-index: 0;
+  width: 80.3347280334%;
+  height: 145px;
+  margin: 22px 0 0 10.03923%;
+  background-color: rgba(54, 40, 40, 0.53);
+  box-shadow: 10px 12px 1px 3px rgba(0, 0, 0, .5);
+}
+
+.element-2 {
+  width: 98.2291666666%;
+  height: 485px;
+  margin: 12px 0 0 1.77083%;
+  background-color: rgb(255, 255, 255);
+}
